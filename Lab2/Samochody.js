8173562ae79698f68b844288b1be4a474a4978ce
Marcin Dawidowski <marcin.dawidowskipl@gmail.com>
2018-03-06 18:06:03 +0100

    skonczone Zad z Lab2

--- Lab2/Samochody.js
+++ Lab2/Samochody.js
@@ -1,10 +1,56 @@
-var pojazd = {}
+var Cars = {};
 
-pojazd.module = (function(){
-      return{
-        sayName: function(name, model){
-            return "Samochod: " + name + " " + model;
-        }
-    };
-}());
-console.log(pojazd.module.sayName("Fiat","125p"));
+Cars = (function() {
+  "use strict";
+  var CarInterface = {
+    brands: [
+      "Audi",
+      "BMW",
+      "Toyota",
+      "Porsche",
+      "Fiat",
+      "Aston Martin",
+      "Dacia",
+      "Mercedes",
+      "Ford",
+      "Seat",
+      "VW",
+      "Rover",
+      "Polones",
+      "Opel",
+      "Daihatsu"
+    ],
+    typeofengine: [
+      "Petrol", "Diesel", "Gas", "Hybrid", "Electric"
+    ],
+    showBrands: function() {
+      return CarInterface.brands.join(", ");
+    },
+    showTypes: function() {
+      return CarInterface.typeofengine.join(", ");
+    },
+    add: function(brand) {
+      CarInterface.brands.push(brand);
+    },
+    delete: function(brand) {
+      const carId = CarInterface.brands.indexOf(brand)
+      if (carId >= 0) {
+        CarInterface.brands.splice(carId, 1);
+      }
+    },
+    edit: function(brand, newbrand) {
+      const carId = CarInterface.brands.indexOf(brand);
+      if (carId !== -1) {
+        CarInterface.brands[carId] = newbrand;
+      }
+    }
+  };
+  CarInterface.brands.sort();
+  return CarInterface;
+})();
+
+Cars.add("McLaren");
+Cars.delete("Daihatsu");
+Cars.edit("Polones", "Polonez")
+console.log(Cars.showTypes());
+console.log(Cars.showBrands());
