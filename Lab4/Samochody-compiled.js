1aea4d943aa136f35660ff8e7269b6ed6e7dd425
Marcin Dawidowski <marcin.dawidowskipl@gmail.com>
2018-03-27 20:52:32 +0200

    aktualizacja przetranspilowanego pliku Samochody.js

--- Lab4/Samochody-compiled.js
+++ Lab4/Samochody-compiled.js
@@ -7,10 +7,10 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 // Klasy ze swojej dziedziny w ES6 zawierające elementy z poprzednich zadań
 // lambda - TAK
 // classes + dziedziczenie - TAK
-// template strings -
+// template strings - TAK
 // default + rest + spread -
 // let, const - TAK
-// interators + for..of -
+// iterators + for..of -
 var modelsAM = [" DB9", " DB9 Coupé", " DB9 Volante", " V8 Vantage", " V8 Vantage Roadster", " V12 Vantage", " Rapide", " Vanquish", " V12 Vantage S", " Lagonda II", " DB11"];
 
 var Car = function () {
@@ -39,14 +39,24 @@ function isOlder(car1, car2) {
     var older = function older(a, b) {
       return a.yearfounded < b.yearfounded ? a : b;
     };
+    var newer = function newer(a, b) {
+      return a.yearfounded > b.yearfounded ? a : b;
+    };
     var isolder = older(car1, car2);
-    return console.log(isolder.brand + " is older");
+    var isnewer = newer(car1, car2);
+    return console.log(isolder.brand + " is older than " + isnewer.brand + ". The difference is " + (isnewer.yearfounded - isolder.yearfounded) + " years");
   }
   return console.log("Brands are the same age");
 }
+
+function gotF1Car(car) {
+  return console.log("It is " + car.hasF1Team + " that " + car.brand + " has F1 team");;
+}
+
 var AstonMartin = new Car("Aston Martin", "mostly petrol", "Great Britain", 1913, true);
 var Mclaren = new Car("Mclaren", "mostly petrol", "Great Britain", 1963, true);
 var Arrinera = new Car("Arrinera", "petrol", "Poland", 2008, false);
 
 console.log("Modele Aston Martin:" + AstonMartin.showModels(modelsAM));
 isOlder(Arrinera, AstonMartin);
+gotF1Car(Mclaren);
