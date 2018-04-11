92583d9621343912bf0053fbc73dc45109a5bd8f
Marcin Dawidowski <marcin.dawidowskipl@gmail.com>
2018-04-11 18:52:39 +0200

    Zad1 ukończone, Zad2 początki

--- Lab5/Zad1/search.js
+++ Lab5/Zad1/search.js
@@ -1,17 +1,21 @@
+// Zad1
+// Poukładać i zwizualizować wyniki z wyszukiwania WikiSearch
+
 var q = $('#q');
 var keyups = Rx.Observable.fromEvent(q, 'keyup');
 keyups.throttleTime(500).map(function(x) {
   return q.val();
-})
-.switchMap(function(x) {
+}).switchMap(function(x) {
   return searchWikipedia(x);
-})
-.do(function(x) {
-  console.log(x);
-})
-.subscribe(function(x) {
-  $('#results').text(x);
-});
+}).do
+  (function(x) {
+    console.log(x);
+  }).subscribe(function(x) {
+    for (var i = 0; i < x[1].length; i++) {
+      x[1][i] = `- <a href="${x[3][i]}"> ${x[1][i]} </a> <br>`
+    }
+    $('#results').html(x[1]);
+  });
 
 function searchWikipedia(term) {
   return $.ajax({
@@ -22,5 +26,6 @@ function searchWikipedia(term) {
       format: 'json',
       search: term
     }
+
   }).promise();
 }
--- Lab5/gra.js
+++ Lab5/gra.js
@@ -1,7 +1,3 @@
-// Zad1
-
-// Poukładać i zwizualizować wyniki z wyszukiwania WikiSearch
-
 // Zad2 Użyć Rx
 
 // Zrobić grę: dwa kwadraty w divie -> jeden sterowanie klawiszami, drugi automatycznie.
