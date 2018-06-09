80465bd37da40bdf2934fb44db889ec170bb3d9e
Marcin Dawidowski <mdawidowski@sigma.ug.edu.pl>
2018-06-09 10:09:52 +0200

    dodano .gitignore

--- Aplikacja/app.js
+++ Aplikacja/app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(cars))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+class Car {
+  constructor(brand, country) {
+    this.brand = brand;
+    this.country = country;
+  }
+
+let cars = [new Car('Aston Martin', '1913'), new Car('Mazda', '1920'), new Car('Ford', '1903')];
